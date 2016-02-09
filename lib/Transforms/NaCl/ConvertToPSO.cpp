87963431ed9037fc32fee88e18bdfbf595f0a854
Mark Seaborn <mseaborn@chromium.org>
2016-02-09 10:01:01 -0800

    PNaCl dynamic linking: Handle imports referenced by global variables
    Add support for importing GlobalValues when they are referenced by
    global variables.  This builds up an array of pointers to locations
    within global variables that must be filled out (relocated) when a
    module is loaded.
    
    Note that this doesn't yet handle importing GlobalValues when they are
    referenced directly within functions.  I'll implement that in a
    follow-on change.
    
    This change simplifies existing export processing slightly by using
    LLVM's ConstantDataArray::getString() for creating a string literal
    with a null terminator (used within the createNameString() function
    that I have split out).
    
    BUG=https://bugs.chromium.org/p/nativeclient/issues/detail?id=4351
    TEST=run_psl_symbols_test_irt in https://codereview.chromium.org/1655383002
    R=phosek@google.com
    
    Review URL: https://codereview.chromium.org/1661733002 .

--- lib/Transforms/NaCl/ConvertToPSO.cpp
+++ lib/Transforms/NaCl/ConvertToPSO.cpp
@@ -20,10 +20,14 @@
 // look up symbols that the module exports and supply definitions of
 // symbols that a module imports.
 //
-// Currently, this pass only implements exporting of symbols.  The
-// following features are not implemented yet:
+// Currently, this pass implements:
 //
-//  * Importing symbols
+//  * Exporting symbols
+//  * Importing symbols when referenced by global variable initializers
+//
+// The following features are not implemented yet:
+//
+//  * Importing symbols when referenced by functions
 //  * Building a hash table of exported symbols to allow O(1)-time lookup
 //  * Support for thread-local variables
 //
@@ -33,6 +37,7 @@
 #include "llvm/IR/DataLayout.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/NaCl.h"
 
 using namespace llvm;
@@ -48,6 +53,34 @@ namespace {
 
     virtual bool runOnModule(Module &M);
   };
+
+  // This takes a SimpleElement from FlattenGlobals' normal form.  If the
+  // SimpleElement is a reference to a GlobalValue, it returns the
+  // GlobalValue along with its addend.  Otherwise, it returns nullptr.
+  GlobalValue *getReference(Constant *Init, uint64_t *Addend) {
+    *Addend = 0;
+    if (isa<ArrayType>(Init->getType()))
+      return nullptr;
+    if (auto CE = dyn_cast<ConstantExpr>(Init)) {
+      if (CE->getOpcode() == Instruction::Add) {
+        if (auto CI = dyn_cast<ConstantInt>(CE->getOperand(1))) {
+          if (auto Op0 = dyn_cast<ConstantExpr>(CE->getOperand(0))) {
+            CE = Op0;
+            *Addend = CI->getSExtValue();
+          }
+        }
+      }
+      if (CE->getOpcode() == Instruction::PtrToInt) {
+        if (auto GV = dyn_cast<GlobalValue>(CE->getOperand(0))) {
+          if (!GV->isDeclaration())
+            return nullptr;
+          return GV;
+        }
+      }
+    }
+    errs() << "Initializer value not handled: " << *Init << "\n";
+    report_fatal_error("ConvertToPSO: Value is not a SimpleElement");
+  }
 }
 
 char ConvertToPSO::ID = 0;
@@ -60,56 +93,134 @@ bool ConvertToPSO::runOnModule(Module &M) {
   Type *PtrType = Type::getInt8Ty(M.getContext())->getPointerTo();
   Type *IntPtrType = DL.getIntPtrType(M.getContext());
 
+  // Creates a new GlobalVariable containing the name of Val.  Uses Name
+  // for the name of the GlobalVariable.
+  auto createNameString = [&](Value *Val, const char *Name) {
+    Constant *Str = ConstantDataArray::getString(
+        M.getContext(), Val->getName());
+    Constant *StrVar = new GlobalVariable(M, Str->getType(), true,
+                                          GlobalValue::InternalLinkage,
+                                          Str, Name);
+    return ConstantExpr::getBitCast(StrVar, PtrType);
+  };
+
+  // In order to simplify the task of processing relocations inside
+  // GlobalVariables' initializers, we first run the FlattenGlobals pass to
+  // reduce initializers to a simple normal form.  This reduces the number
+  // of cases we need to handle, and it allows us to iterate over the
+  // initializers instead of needing to recurse.
+  ModulePass *Pass = createFlattenGlobalsPass();
+  Pass->runOnModule(M);
+  delete Pass;
+
+  // Process imports.
+  SmallVector<Constant *, 32> ImportNames;
+  SmallVector<Constant *, 32> ImportPtrs;
+  for (GlobalVariable &Var : M.globals()) {
+    if (!Var.hasInitializer())
+      continue;
+    Constant *Init = Var.getInitializer();
+    if (auto CS = dyn_cast<ConstantStruct>(Init)) {
+      // The initializer is a CompoundElement (i.e. a struct containing
+      // SimpleElements).
+      SmallVector<Constant *, 32> Elements;
+      bool Modified = false;
+
+      for (unsigned I = 0; I < CS->getNumOperands(); ++I) {
+        Constant *Element = CS->getOperand(I);
+        uint64_t Addend;
+        if (auto GV = getReference(Element, &Addend)) {
+          // Calculate the address that needs relocating.
+          Value *Indexes[] = {
+            ConstantInt::get(M.getContext(), APInt(32, 0)),
+            ConstantInt::get(M.getContext(), APInt(32, I)),
+          };
+          Constant *Addr = ConstantExpr::getGetElementPtr(
+              Init->getType(), &Var, Indexes);
+          // Record the relocation.
+          ImportPtrs.push_back(ConstantExpr::getBitCast(Addr, PtrType));
+          ImportNames.push_back(createNameString(GV, "import_str"));
+          // Replace the original reference with the addend value.
+          Element = ConstantInt::get(Element->getType(), Addend);
+          Modified = true;
+        }
+        Elements.push_back(Element);
+      }
+
+      if (Modified) {
+        // Note that the resulting initializer will not follow
+        // FlattenGlobals' normal form, because it will contain i32s rather
+        // than i8 arrays.  However, the later pass of FlattenGlobals will
+        // restore the normal form.
+        Var.setInitializer(
+            ConstantStruct::getAnon(M.getContext(), Elements, true));
+      }
+    } else {
+      // The initializer is a single SimpleElement.
+      uint64_t Addend;
+      if (auto GV = getReference(Init, &Addend)) {
+        // Record the relocation.
+        ImportPtrs.push_back(ConstantExpr::getBitCast(&Var, PtrType));
+        ImportNames.push_back(createNameString(GV, "import_str"));
+        // Replace the original reference with the addend value.
+        Var.setInitializer(ConstantInt::get(Init->getType(), Addend));
+      }
+    }
+  }
+
+  // Process exports.
   SmallVector<Constant *, 32> ExportNames;
   SmallVector<Constant *, 32> ExportPtrs;
 
   auto processGlobalValue = [&](GlobalValue &GV) {
-    if (GV.isDeclaration() ||
-        GV.getLinkage() != GlobalValue::ExternalLinkage)
+    if (GV.isDeclaration()) {
+      // Aside from intrinsics, we should have handled any imported
+      // references already.
+      if (auto Func = dyn_cast<Function>(&GV)) {
+        if (Func->isIntrinsic())
+          return;
+      }
+      GV.removeDeadConstantUsers();
+      assert(GV.use_empty());
+      GV.eraseFromParent();
+      return;
+    }
+
+    if (GV.getLinkage() != GlobalValue::ExternalLinkage)
       return;
 
-    // Make a copy of the name to add a null terminator.
-    StringRef Name = GV.getName();
-    std::string NameWithNull;
-    NameWithNull.reserve(Name.size() + 1);
-    NameWithNull = Name;
-    NameWithNull.push_back(0);
-    const uint8_t *NameData =
-        reinterpret_cast<const uint8_t *>(NameWithNull.data());
-    Constant *Str = ConstantDataArray::get(
-        M.getContext(),
-        ArrayRef<uint8_t>(NameData, NameData + NameWithNull.size()));
-    Constant *StrVar = new GlobalVariable(M, Str->getType(), true,
-                                          GlobalValue::InternalLinkage,
-                                          Str, "export_str");
-    ExportNames.push_back(ConstantExpr::getBitCast(StrVar, PtrType));
+    ExportNames.push_back(createNameString(&GV, "export_str"));
     ExportPtrs.push_back(ConstantExpr::getBitCast(&GV, PtrType));
     GV.setLinkage(GlobalValue::InternalLinkage);
   };
 
-  for (Function &Func : M.functions())
-    processGlobalValue(Func);
-  for (GlobalValue &Var : M.globals())
-    processGlobalValue(Var);
+  for (Module::iterator Iter = M.begin(); Iter != M.end(); ) {
+    processGlobalValue(*Iter++);
+  }
+  for (Module::global_iterator Iter = M.global_begin();
+       Iter != M.global_end(); ) {
+    processGlobalValue(*Iter++);
+  }
 
-  // Set up array of exported symbol names.
-  Constant *ExportNamesArray = ConstantArray::get(
-      ArrayType::get(PtrType, ExportNames.size()), ExportNames);
-  Constant *ExportNamesVar = new GlobalVariable(
-      M, ExportNamesArray->getType(), true, GlobalValue::InternalLinkage,
-      ExportNamesArray, "export_names");
-
-  // Set up array of exported symbol values.
-  Constant *ExportPtrsArray = ConstantArray::get(
-      ArrayType::get(PtrType, ExportPtrs.size()), ExportPtrs);
-  Constant *ExportPtrsVar = new GlobalVariable(
-      M, ExportPtrsArray->getType(), true, GlobalValue::InternalLinkage,
-      ExportPtrsArray, "export_ptrs");
+  // Set up an array of pointers.
+  auto createArray = [&](const char *Name,
+                         SmallVectorImpl<Constant *> *Array) -> Constant * {
+    Constant *Contents = ConstantArray::get(
+        ArrayType::get(PtrType, Array->size()), *Array);
+    return new GlobalVariable(
+        M, Contents->getType(), true, GlobalValue::InternalLinkage,
+        Contents, Name);
+  };
 
   Constant *PsoRoot[] = {
-    ExportPtrsVar,
-    ExportNamesVar,
+    // Exports
+    createArray("export_ptrs", &ExportPtrs),
+    createArray("export_names", &ExportNames),
     ConstantInt::get(IntPtrType, ExportPtrs.size()),
+    // Imports
+    createArray("import_ptrs", &ImportPtrs),
+    createArray("import_names", &ImportNames),
+    ConstantInt::get(IntPtrType, ImportPtrs.size()),
   };
   Constant *PsoRootConst = ConstantStruct::getAnon(PsoRoot);
   new GlobalVariable(
--- test/Transforms/NaCl/convert-to-pso.ll
+++ test/Transforms/NaCl/convert-to-pso.ll
@@ -2,8 +2,6 @@
 ; RUN: opt < %s -convert-to-pso -pnacl-abi-simplify-postopt \
 ; RUN:     -verify-pnaclabi-module -verify-pnaclabi-functions -S | FileCheck %s
 
-; CHECK: @__pnacl_pso_root = constant
-
 target datalayout = "p:32:32:32"
 
 
@@ -28,3 +26,34 @@ define i8* @my_read_tp() {
   %thread_pointer = call i8* @llvm.nacl.read.tp()
   ret i8* %thread_pointer
 }
+
+
+; Test importing values.
+@imported_var = external global i32
+@imported_var_addend = external global i32
+@imported_var2 = external global i32
+@imported_var3 = external global i32
+
+; Test that an import is replaced with an addend.  Here, the addend is zero.
+@reloc_var = global i32* @imported_var
+; CHECK: @reloc_var = internal global [4 x i8] zeroinitializer
+
+; Test a non-zero addend.
+@reloc_var_addend = global i32* getelementptr (i32, i32* @imported_var, i32 1)
+; CHECK: @reloc_var_addend = internal global [4 x i8] c"\04\00\00\00"
+
+; Test multiple imports within a single global variable initializer.
+@reloc_var_offset = global [4 x i32*] [
+    i32* null,
+    i32* getelementptr (i32, i32* @imported_var2, i32 1),
+    i32* getelementptr (i32, i32* @imported_var3, i32 2),
+    i32* inttoptr (i32 255 to i32*)]
+; CHECK: @reloc_var_offset = internal global [16 x i8] c"\00\00\00\00\04\00\00\00\08\00\00\00\FF\00\00\00"
+
+; References to module-local variables should not be modified.
+@local_var = internal global i32 0
+@local_reloc_var = global i32* @local_var
+; CHECK: @local_reloc_var = internal global i32 ptrtoint ([4 x i8]* @local_var to i32)
+
+
+; CHECK: @__pnacl_pso_root = constant
